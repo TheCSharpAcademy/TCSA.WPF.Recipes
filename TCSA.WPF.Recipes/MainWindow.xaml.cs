02974f9747f88f7406683fad4f54484496e6ea74
Pablo <thecsharpacademy@gmail.com>
2024-12-30 11:46:35 +1000

    list of recipes

--- TCSA.WPF.Recipes/MainWindow.xaml.cs
+++ TCSA.WPF.Recipes/MainWindow.xaml.cs
@@ -1,24 +1,48 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace TCSA.WPF.Recipes
+namespace TCSA.WPF.Recipes;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public ObservableCollection<Recipe> Recipes { get; set; }
+
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+
+        Recipes = new ObservableCollection<Recipe>
         {
-            InitializeComponent();
+            new Recipe { Name = "Spaghetti Bolognese", Details = "Ingredients: Spaghetti, Ground Beef, Tomato Sauce.\nSteps: Cook pasta, make sauce, mix together." },
+            new Recipe { Name = "Pancakes", Details = "Ingredients: Flour, Eggs, Milk.\nSteps: Mix ingredients, cook on a skillet." }
+        };
+
+        RecipeList.ItemsSource = Recipes;
+    }
+
+    private void RecipeList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+        if (RecipeList.SelectedItem is Recipe selectedRecipe)
+        {
+            RecipeDetails.Text = selectedRecipe.Details;
+        }
+        else
+        {
+            RecipeDetails.Text = "Select a recipe to view details.";
         }
     }
+
+    private void AddRecipe_Click(object sender, RoutedEventArgs e)
+    {
+        Recipes.Add(new Recipe { Name = "New Recipe", Details = "Details about the recipe." });
+    }
+}
+
+public class Recipe
+{
+    public string Name { get; set; }
+    public string Details { get; set; }
 }
