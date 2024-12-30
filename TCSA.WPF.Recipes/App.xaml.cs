fc612de704ea7f0f9705a056dc1c8eea900014cc
Pablo <thecsharpacademy@gmail.com>
2024-12-30 11:40:28 +1000

    Add project files.

---
+++ TCSA.WPF.Recipes/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TCSA.WPF.Recipes
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
